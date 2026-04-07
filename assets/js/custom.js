32abf50809b05905bc5b2f81dfa26ecaa8e2dd09
atif-appsnation <atif.hanif@appsnation.co>
2026-04-07 21:00:56 +0500

    Responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -963,3 +963,38 @@ $('.awards-slider').owlCarousel({
 		},
 	}
 });
+
+$('.client-logo-slider').owlCarousel({
+	items: 4,
+	loop: true,
+	autoplay: true,
+	centre: true,
+	margin: 15,
+	nav: false,
+	dots: false,
+	autoplayTimeout: 3000,
+	autoplayHoverPause: true,
+	smartSpeed: 2000,
+	responsiveClass: true,
+	rewind: true,
+	responsive: {
+		0: {
+			items: 3
+		},
+		520: {
+			items: 3
+		},
+		768: {
+			items: 3
+		},
+		1200: {
+			items: 3
+		},
+		1400: {
+			items: 4
+		},
+		1600: {
+			items: 4
+		},
+	}
+});
